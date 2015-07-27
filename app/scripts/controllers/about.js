ffb736eb3ac369b0bab6e13215425a2bbb887535
Frederic Yesid Peña Sánchez <fredericpena@hotmail.com>
2015-07-27 17:42:01 -0500

    * Estilos Iniciales

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name torneosPslApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the torneosPslApp
+ */
+angular.module('torneosPslApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
